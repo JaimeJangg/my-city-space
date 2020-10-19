4bc8c0afb4de056670d4a4f5882008e5c756ff14
Jaime Rueda <jaime_rueda10@hotmail.com>
2020-10-19 16:17:59 -0500

    mejorando el footer dos

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,8 +21,6 @@ const IndexPage = () => (
       </div>
       <Wave />
     </div>
-    {/* <div className="cards"> */}
-      {/* <h2 className="interceptionCards">Inovacion tecnologica para mejorar la vida de las sociedades.</h2> */}
       <SectionInnovation
         title="Buscamos construir las estructuras del futuro."
         text="Sabemos que el rezago de las tecnologias del momento
@@ -31,30 +29,21 @@ const IndexPage = () => (
           de lo que implica esto y como meta lograr que las sociedades y negocios sean exitosos y utilicen las tecnologias
           de forma correcta."
       />
-    {/* </div> */}
     <SectionWave 
-      // image={require('../images/projects.jpeg')}
       title="Nuestros Proyectos..."
     />
-    {/* <div className="cards"> */}
       <CardsProjects
         linkCard="Ver Proyectos"
       />
-    {/* </div> */}
     <SectionWaveServices
-      // image={require('../images/services.jpeg')}
       title="Nuestros Servicios..."
       text="Estan hechos para que tus negocios, proyectos y tu gente sean un exito, siempre con
         el toque mas humano posible, nuestro equipo es tu equipo."
     />
-    {/* <div className="cards"> */}
       <div className="interceptionCards">
         <div className="servicesBackground" />
-        <div>
-          Proximamente podras encontrar nuestro listado de servicios segun tu localilad.
-        </div>
+          <h2 className="sectionTitle">Proximamente podras encontrar nuestro listado de servicios segun tu localilad.</h2>
       </div>
-    {/* </div> */}
   </Layout>
 )
 
